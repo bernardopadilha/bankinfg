833e1260b11a169f0bcdf7fbb1a341e1ae9e1244
Bernardo <bernardo_limas@hotmail.com>
2024-07-04 15:44:06 -0300

    Implement PlaidLink and Dwolla

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,8 +3,6 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 
 const SingUp = async () => {
   const loggedInUser = await getLoggedInUser()
-
-  console.log(loggedInUser)
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-up" />
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,6 +15,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 
 const AuthForm = ({ type }: {type: string}) => {
@@ -38,8 +39,21 @@ const AuthForm = ({ type }: {type: string}) => {
     try {
       // Sign up with AppWrite & create plain link
 
+      
       if (type === 'sign-up') {
-        const newUser = await signUp(data)
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+        const newUser = await signUp(userData)
 
         setUser(newUser)
       }
@@ -95,9 +109,9 @@ const AuthForm = ({ type }: {type: string}) => {
       </header>
       {user ? (
         <div className='flex flex-col gap-4'>
-          {/* PlaidLink */}
+          <PlaidLink user={user} variant="primary" />
         </div>
-      ) : (
+      ) : ( 
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
